d4c96522cc56c227efeb157f272719bea7100fae
faizzaidi23 <786faizzaidi@gmail.com>
2025-11-22 19:03:09 +0530

    feat:added the flush to print the things in the sequence as they should get printed

---
+++ src/main/kotlin/CoroutineBuilders/6_coroutineBuilders.kt
@@ -0,0 +1,16 @@
+package org.example.CoroutineBuilders
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun main()= runBlocking { //Creates a blocking coroutine that executes in the current thread i.e  the main thread
+    println("Main Program starts:${Thread.currentThread().name}") //main thread
+    launch{  //Thread:main -->this is a child coroutine so its takes the scope of its parent that is the runBlocking i.e it also runs on the main Thread too
+        println("Fake work started:${Thread.currentThread().name}")
+        delay(1000)
+        println("Fake work finished:${Thread.currentThread().name}")
+    }
+    delay(2000)
+    println("Main program ends:${Thread.currentThread().name}")
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,17 +4,18 @@ import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlin.concurrent.thread
 
-fun main(){
+fun main() {
+    println("Main program starts:${Thread.currentThread().name}")
+    System.out.flush()
 
-    println("Main program starts:${Thread.currentThread().name}") //This executes on the main thread
-
-
-    thread{ //This is the background thread which is running on parallel on the main thread
+    thread {
         println("Fake work starts:${Thread.currentThread().name}")
-        Thread.sleep(1000) //Pretending to do some work on the background thread like the file upload
+        System.out.flush()
+        Thread.sleep(10000)
         println("Fake work ends:${Thread.currentThread().name}")
+        System.out.flush()
     }
 
     println("Main programs ends:${Thread.currentThread().name}")
+    System.out.flush()
 }
-
