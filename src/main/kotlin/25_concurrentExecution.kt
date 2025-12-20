c273bbea667a852db067642b7db53994a4a250c7
faizzaidi23 <786faizzaidi@gmail.com>
2025-12-20 10:48:09 +0530

    more about concurrentExecution

--- src/main/kotlin/25_concurrentExecution.kt
+++ src/main/kotlin/25_concurrentExecution.kt
@@ -4,6 +4,21 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import kotlin.system.measureTimeMillis
 
+
+/*
+Here the async coroutine builder will launch a coroutine on the main thread too
+since the parent coroutine is the runBlocking and the async is the child coroutine
+it will be running on the main thread
+They will be returning the object of Deferred<String>--->Here deferred means the result we will be getting lately
+but since the suspend functions have delay in them
+when delay() will be encountered then the coroutine will be suspended and the second coroutine will get hold of the main thread
+and then the delay() is encountered on the second suspending function then the main thread is free
+and after 1 second when the delay of both the suspending functions is completed then it will be doing to the returning
+
+
+*******The main thing here is we do not know which coroutine will get the coroutine after the resuming of the coroutine
+
+*/
 fun main()=runBlocking{
 
     println("Main function starts on the ${Thread.currentThread().name}")
